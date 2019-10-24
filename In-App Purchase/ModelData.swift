9f925f2f1a11c58d15f63b8f93a868dd790255bc
mohamedosama2074 <iosengks74@gmail.com>
2019-10-24 12:01:31 +0200

    In-App Purchase

---
+++ In-App Purchase/ModelData.swift
@@ -0,0 +1,29 @@
+//
+//  ModelData.swift
+//  In-App Purchase
+//
+//  Created by Mohamed on 10/23/19.
+//  Copyright © 2019 Mohamed74. All rights reserved.
+//
+
+import Foundation
+
+class ModelData{
+    
+    
+    //MARK: - handle free quotes
+    
+    class func getFreeQuotes()->[String]{
+        
+        return ["mohamed osama is iOS developer" , "Ahmed osama is Android developer" , "Yousef is a Full Stack developer"]
+    }
+    
+    //MARK: - handle priemuim quotes
+    
+    
+    class func getPriemuimQuotes()->[String]{
+        
+        return ["ibrahim osama is iOS developer" , "Samir osama is Android developer" , "Alaa is a Full Stack developer"]
+    }
+    
+}
--- In-App Purchase/ViewController.swift
+++ In-App Purchase/ViewController.swift
@@ -7,14 +7,137 @@
 //
 
 import UIKit
+import StoreKit
 
-class ViewController: UIViewController {
+class TableViewController: UITableViewController,SKPaymentTransactionObserver{
+  
+    
 
+    let appID = "Mohamed-osama.In-App-Purchase"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        for conuter in ModelData.getFreeQuotes(){
+            
+            print(conuter)
+        }
+        
     }
-
-
+    
+    //MARK:- IBActions
+    
+    @IBAction func restoreButtonPressed(_ sender: UIBarButtonItem) {
+        
+        SKPaymentQueue.default().restoreCompletedTransactions()
+    }
+    
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        
+        
+        return ModelData.getFreeQuotes().count + 1
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellItem", for: indexPath)
+        
+        if indexPath.row < ModelData.getFreeQuotes().count{
+            
+            cell.textLabel?.text = ModelData.getFreeQuotes()[indexPath.row]
+            
+        }else {
+            
+            cell.textLabel?.text = "Get more quotes"
+            cell.accessoryType = .disclosureIndicator
+            cell.textLabel?.textColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1)
+        }
+        
+        return cell
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if indexPath.row == ModelData.getFreeQuotes().count{
+            
+            buyMoreQuotes()
+        }
+        
+        
+    }
+    
+    func buyMoreQuotes(){
+        
+        if SKPaymentQueue.canMakePayments(){
+            
+            let paymentRequest = SKMutablePayment()
+            
+            paymentRequest.productIdentifier = appID
+            
+            SKPaymentQueue.default().add(paymentRequest)
+            
+        }else{
+            
+            
+            
+        }
+        
+    }
+    
+    func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
+          
+                
+        for transaction in transactions {
+            
+            switch transaction.transactionState {
+            case .purchased:
+                
+                // payment successfully
+                
+                print("payment successfully")
+                showPriemumQuotes()
+                SKPaymentQueue.default().finishTransaction(transaction)
+                break
+                
+            case .failed:
+                
+                // payment failed
+                print("payment failed")
+                break
+                
+            case .restored:
+                SKPaymentQueue.default().restoreCompletedTransactions()
+                
+                break
+            default:
+                break
+            }
+        }
+        
+      }
+    
+    
+    
+    func showPriemumQuotes(){
+        
+        var quotes = ModelData.getFreeQuotes()
+        
+        quotes.append(contentsOf: ModelData.getPriemuimQuotes())
+        
+        tableView.reloadData()
+    }
+    
 }
 
